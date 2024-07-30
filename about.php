e873defde99b5d12a8582661dec3c489cd46e54f
BARATH M <129058869+mbarath19@users.noreply.github.com>
2024-07-30 14:42:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,76 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>"Choose our eLearning platform for an unparalleled educational experience tailored to your schedule and learning pace. With a diverse range of courses, interactive modules, and expert instructors, we offer flexibility and convenience without compromising quality. Whether you're a lifelong learner or advancing your career, our user-friendly interface and cutting-edge technology ensure a seamless journey towards your educational goals. Join us and discover a new way to learn, anytime, anywhere."</p>
+        
+      </div>
+
+   </div>
+
+   <div class="box-container">
+
+        </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
